eed9e1a42fb49b39377a2ebb3ce663503cfa6bd5
Andrew <73917861+andrewnx@users.noreply.github.com>
2023-05-24 20:24:06 -0230

    2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h1 class="shadow-text">{{ title }}</h1>
-    <p class="shadow-text">{{ description }}</p>
+    <div class="shadow-text" v-html="description"></div>
     <button @click="purchase">{{ buttonText }}</button>
   </div>
 </template>
@@ -12,7 +12,7 @@ import backgroundImage from "@/assets/chocolate-fruit.jpg";
 export default {
   data() {
     return {
-      title: 'For Sale: Chocolate covered fruit baskets',
+      title: 'Treat Your Senses: Our Premium Chocolate-Drizzled Fruit Baskets',
       description: '<p>Unveiling the epitome of luxury and elegance with our Premium Chocolate-Drizzled Fruit Baskets. Specially curated for discerning palates, each basket showcases a rich assortment of the season\'s finest fruits, expertly hand-dipped in velvety, indulgent chocolate. The harmonic balance between the sweetness of the fruits and the creaminess of the chocolate creates an exquisite blend of flavors that indulges your senses and elevates your experience.</p><p>Our focus on quality is relentless and uncompromising. We hand-select every piece of fruit to ensure optimal ripeness and flavor. Our chocolatiers meticulously drizzle each slice with gourmet chocolate, creating a beautiful and tantalizing presentation. Whether it\'s a gift or a personal indulgence, our Chocolate-Drizzled Fruit Baskets promise an unforgettable experience of taste and refinement, embodying the very essence of luxury.</p>',
       buttonText: 'Buy Now',
       backgroundImage: backgroundImage
@@ -42,6 +42,7 @@ body {
   padding: 0;
   font-family: Arial, sans-serif;
   color: #f0e9e2;
+  background: rgba(0, 0, 0, 0.7);
 }
 
 #app {
@@ -53,7 +54,12 @@ body {
   padding: 1rem;
   box-sizing: border-box;
   text-align: center;
+  background: rgba(0, 0, 0, 0.3);
+  border-radius: 1rem;
+  backdrop-filter: blur(1px);
 }
+
+
 
 .shadow-text {
   text-shadow: 2px 2px 4px #000000;
@@ -64,7 +70,7 @@ h1 {
   color: #b78c6a;
 }
 
-p {
+div.shadow-text {
   font-size: 1.2rem;
   color: #e7d4c1;
   margin: 2rem 0;
@@ -90,7 +96,7 @@ button:hover {
     font-size: 3rem;
   }
 
-  p {
+  div.shadow-text {
     font-size: 1.5rem;
   }
 
@@ -99,3 +105,6 @@ button:hover {
   }
 }
 </style>
+
+
+
